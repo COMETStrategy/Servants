8e5985ae8c4ef623bb34a46da246a493dae4366b
Brett King <Brett.King@cometstrategy.com>
2025-07-26 17:55:29 +1000

    Used from Visual Studio so I have access to CoPilot, Renamed main.cpp to Console.cpp

---
+++ Console/Console.cpp
@@ -0,0 +1,32 @@
+﻿// Console.cpp : Defines the entry point for the application.
+//
+
+#include "Console.h"
+
+
+#include "Services/WebServices.h"
+#include "Services/Logger.h"
+
+int main()
+{
+
+  try {
+
+    comet::WebServices server("~/comet-servants.db");
+
+    COMETLOG("Server is running. Press Ctrl+C to stop.", comet::DEBUGGING);
+
+    server.join();
+  }
+  catch (const std::exception& e) {
+    COMETLOG(std::string("Exception occurred: ") + e.what(), comet::CRITICAL);
+    return 1; // Return a non-zero value to indicate failure
+  }
+  catch (...) {
+    COMETLOG("An unknown error occurred.", comet::CRITICAL);
+    return 1; // Return a non-zero value to indicate failure
+  }
+  COMETLOG(std::string("☑️Completed 👋"), comet::INFO);
+
+  return 0;
+}
